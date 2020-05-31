a63768089e1a05a702852c0ccc35c6cf022ac2da
justin-nicky <justinkjoseph17@gmail.com>
2020-05-31 20:58:27 +0530

    added templates and styles!

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author':'justin',
+        'title':'blog post 1',
+        'content':'writing my first post',
+        'date_posted':'may 31, 2020'
+    },
+    {
+        'author':'justin',
+        'title':'blog post 2',
+        'content':'writing my second post',
+        'date_posted':'may 31, 2020'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog home</h1>')
+    context = { 'posts': posts,}
+    return render(request, 'blog/home.htm', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog-about</h1>')
+    return render(request, 'blog/about.htm')
