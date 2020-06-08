05c7dbb20afa0ab710bd35a5c8258bdbe32987f1
justin-nicky <justinkjoseph17@gmail.com>
2020-06-08 11:23:31 +0530

    Setup a login and logout system for the users

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 def register(request):
@@ -7,9 +8,12 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been created! You can now Login to your account.')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.htm', {'form':form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.htm')
